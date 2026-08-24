3a4081942ee291a59809f283614e0b7b6c50551b
Sarah Bass <69780815+SarahBass@users.noreply.github.com>
2026-08-23 21:04:28 -0700

    Create Sponge.js

---
+++ Sponge.js
@@ -0,0 +1,73 @@
+import Poco from "commodetto/Poco";
+
+console.log("Hello, Gary.");
+
+const render = new Poco(screen);
+
+const black = render.makeColor(0, 0, 0);
+
+const gary = new Poco.PebbleBitmap(1);
+const pet = new Poco.PebbleBitmap(4);
+
+let garyX = render.width;
+let garyY = (render.height - gary.height) / 2;
+let petX = 1;
+
+function draw() {
+
+    render.begin();
+
+    render.fillRectangle(
+        black,
+        0,
+        0,
+        render.width,
+        render.height
+    );
+
+render.drawBitmap(
+        pet,
+        petX,
+        garyY
+    );
+
+    render.drawBitmap(
+        gary,
+        garyX,
+        garyY
+    );
+
+    render.end();
+}
+
+
+// Initial draw
+draw();
+
+
+// Move Gary approximately 30 times per second
+setInterval(() => {
+
+    // Move Gary to the left
+    garyX -= 2;
+
+
+    // If Gary goes completely off the left side,
+    // put him back on the right.
+    if (garyX < -gary.width) {
+
+        garyX = render.width;
+    }
+// Move Pet to the right
+petX += 2;
+
+// If Pet goes completely off the right side,
+// put him back on the left.
+if (petX > render.width) {
+    petX = -pet.width;
+}
+
+    // Redraw everything
+    draw();
+
+}, 33);
