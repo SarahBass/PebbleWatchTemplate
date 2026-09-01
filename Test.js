f486170932f249173c4e3a430210c3b484983331
Sarah Bass <69780815+SarahBass@users.noreply.github.com>
2026-09-01 11:02:43 -0700

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,18 +1,42 @@
 import Poco from "commodetto/Poco";
 
-console.log("Hello, GBitmap.");
-
 const render = new Poco(screen);
 
-const black = render.makeColor(0, 0, 0);
+// ------------------------------------------------------------
+// TEST BITMAP
+// Change this number to test each resource:
+// 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+// ------------------------------------------------------------
 
-const bitmap = new Poco.PebbleBitmap(1);
+const testBitmap = new Poco.PebbleBitmap(2);
+
+
+// ------------------------------------------------------------
+// DRAW
+// ------------------------------------------------------------
 
 render.begin();
-render.fillRectangle(black, 0, 0, render.width, render.height);
-render.drawBitmap(
-    bitmap,
-    (render.width - bitmap.width) / 2,
-    (render.height - bitmap.height) / 2
+
+// Black background
+const black = render.makeColor(0, 0, 0);
+
+render.fillRectangle(
+    black,
+    0,
+    0,
+    render.width,
+    render.height
 );
+
+
+// ------------------------------------------------------------
+// Draw bitmap at top-left
+// ------------------------------------------------------------
+
+render.drawBitmap(
+    testBitmap,
+    0,
+    0
+);
+
 render.end();
